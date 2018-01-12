db0a77be89fc48fc53b188c2043c1e1ac6feee2b
Jesús Tepec <ttpecjn@gmail.com>
2018-01-12 04:05:39 -0600

    Pista de carreras - tutorial modificaciones
    Modificaciones para crea el tutorial
    -Fondo base en horizontal

--- Animaciones/BaseAnimacion.py
+++ Animaciones/BaseAnimacion.py
@@ -7,14 +7,19 @@ ROJO = (255, 20, 20)
 AZUL = (10, 222, 255)
 
 
-
-def dibujarPista():
+def dibujarPistaVertical():
     pygame.draw.rect(pantalla, NEGRO, [160, 0, 160, 620])
     for i in range(10, 610, 20):
         pygame.draw.rect(pantalla, BLANCO, [234, i, 6, 8])
 
+def dibujarPistaHorizontal():
+    pygame.draw.rect(pantalla, NEGRO, [0, 160, 620, 160])
+    for i in range(10, 620, 20):
+        pygame.draw.rect(pantalla, BLANCO, [i, 234, 8, 6])
+
 pygame.init()
 
+#dimensiones = [620, 480]
 dimensiones = [480, 620]
 pantalla = pygame.display.set_mode(dimensiones)
 pygame.display.set_caption("Animaciones")
@@ -25,15 +30,16 @@ auto = [190, 610, 20, 25]
 hecho = False
 reloj = pygame.time.Clock()
 
+x = 190
+y = 0
+
 while not hecho:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             hecho = True
-    # --- LA LÓGICA DEL JUEGO DEBERÍA IR AQUÍ
-
-    # --- EL CÓDIGO DE DIBUJO DEBERÍA IR AQUÍ
     pantalla.fill(VERDE)
-    dibujarPista()
+    dibujarPistaVertical()
+#    dibujarPistaHorizontal()
     pygame.draw.rect(pantalla, ROJO, autos[0])
     pygame.draw.rect(pantalla, ROJO, autos[1])
     pygame.draw.rect(pantalla, AZUL, auto)
