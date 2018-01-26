0d4ae54b03d82e7d6da1cbf877775caeecb6fc66
Jesús Tepec <ttpecjn@gmail.com>
2018-01-26 02:54:51 -0600

    Res. Eventos de teclado - deteccion de colisiones
    -Colision rectangular
    -Control del juego

--- Mandos de juego/Racing.py
+++ Mandos de juego/Racing.py
@@ -2,6 +2,7 @@ import pygame
 import random
 
 NEGRO = (10, 8 ,12)
+COLOR_PISTA2 = (50, 58, 52)
 BLANCO = (255, 255, 255)
 VERDE = (90, 232, 64)
 ROJO = (255, 20, 20)
@@ -13,23 +14,22 @@ def dibujarTexto(screen, texto, pos):
     text = fuente.render(texto, 1, COLOR_FUENTE)
     screen.blit(text, pos)
 
-def dibujarPista():
-    pygame.draw.rect(pantalla, NEGRO, [160, 0, 160, dimensiones[1]])
+def dibujarPista(x_pista, ancho, ColorPista):
+    pygame.draw.rect(pantalla, ColorPista, [x_pista, 0, ancho, dimensiones[1]])
     for i in range(10, dimensiones[1], 20):
         pygame.draw.rect(pantalla, BLANCO, [234, i, 6, 8])
 
 def rectCollider(objeto1, objeto2):
     colisiono = False
-    if (objeto2[0] + objeto2[2] > objeto1[0] and objeto2[0] < objeto1[0] + objeto1[2]):
-        if(objeto2[1] + objeto2[3] > objeto1[1] and objeto2[1] < objeto1[1] + objeto1[3]):
-            colisiono = True
-    if (objeto2[0] < objeto1[0] and objeto2[0] + objeto2[2] > objeto1[0] + objeto1[2]):
-        if (objeto2[1] < objeto1[1] and objeto2[1] + objeto2[3] > objeto1[1] + objeto1[3]):
-            colisiono = True
-    if (objeto1[0] < objeto2[0] and objeto1[0] + objeto1[2] > objeto2[0] + objeto2[2]):
-        if (objeto1[1] < objeto2[1] and objeto1[1] + objeto1[3] > objeto2[1] + objeto2[3]):
-            colisiono = True
+    if objeto2[0] + objeto2[2] > objeto1[0] and objeto2[0] < objeto1[0] + objeto1[2] and objeto2[1] + objeto2[3] > objeto1[1] and objeto2[1] < objeto1[1] + objeto1[3]:
+        colisiono = True
     return colisiono
+'''    if objeto2[0] < objeto1[0] and objeto2[0] + objeto2[2] > objeto1[0] + objeto1[2] and objeto2[1] < objeto1[1] and objeto2[1] + objeto2[3] > objeto1[1] + objeto1[3]:
+        colisiono = True
+        print("#########2")
+    if objeto1[0] < objeto2[0] and objeto1[0] + objeto1[2] > objeto2[0] + objeto2[2] and objeto1[1] < objeto2[1] and objeto1[1] + objeto1[3] > objeto2[1] + objeto2[3]:
+        colisiono = True
+        print("#########3")'''
 
 pygame.init()
 
@@ -43,16 +43,18 @@ auto = [185, 600, 40, 54]
 x_speed = 0
 y_speed = 0
 
-velocidad = 4
+inicio_pista = 160
+ancho_pista = 160
+
+velocidad = 2
 
 game_over = False
 salir = False
 reloj = pygame.time.Clock()
 
-while not game_over:
+while not salir:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
-            game_over = True
             salir = True
         if evento.type == pygame.KEYDOWN:
             if evento.key == pygame.K_LEFT:
@@ -73,16 +75,23 @@ while not game_over:
             if evento.key == pygame.K_DOWN:
                 y_speed = 0
     pantalla.fill(VERDE)
-    dibujarPista()
+    dibujarPista(inicio_pista, ancho_pista, NEGRO)
     pygame.draw.rect(pantalla, AZUL, auto)
-    if (auto[0] + 40) < 319 and auto[0] > 161:
+    if auto[0] > inicio_pista + 1 and (auto[0] + auto[2]) < (inicio_pista + ancho_pista) -1:
         auto[0] += x_speed
     else:
         x_speed *= -1
-        auto[0] = 162
+        if (auto[0] + auto[2]) >= (inicio_pista + ancho_pista) - 1:
+            auto[0] = (inicio_pista + ancho_pista) - auto[2] - 2
+        elif auto[0] <= inicio_pista + 1:
+            auto[0] = inicio_pista + 2
+
     auto[1] += y_speed
     for autoObjeto in autos:
         game_over = rectCollider(auto, autoObjeto)
+        if(game_over == True):
+            salir = True
+            break
         if autoObjeto[1] > dimensiones[1]:
             autoObjeto[1] = random.randrange(-150, -100)
         pygame.draw.rect(pantalla, ROJO, autoObjeto)
@@ -91,15 +100,17 @@ while not game_over:
     pygame.display.flip()
     reloj.tick(60)
 
-if game_over == True:
+if game_over == True and salir == True:
     game_over = False
     while not game_over:
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 game_over = True
         pantalla.fill(VERDE)
-        dibujarPista()
-        dibujarTexto(pantalla, "GAME OVER", [140, 400])
+        dibujarPista(inicio_pista, ancho_pista, COLOR_PISTA2)
+        dibujarTexto(pantalla, "GAME OVER", [100, 350])
         pygame.display.flip()
         reloj.tick(60)
+    pygame.quit()
+elif salir == True:
     pygame.quit()
